93716b86e5096c9ab8d27b6f8ffdcb920d37d551
leticiaoli <lesilvaoliveira03@gmail.com>
2023-09-18 20:09:46 -0300

    finalized contact page

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Contact.scss";
+
+const Contact = () => {
+  return (
+    <div className="containerFull contact">
+      <div className="containerCenter">
+        <div className="contact__content">
+          <p className="contact__content-paragraph">
+            Essa loja foi construída usando uma das nossas soluções da
+            plataforma VTEX. tenha a sua.
+          </p>
+          <p className="contact__content-text">Entre em contato</p>
+          <p className="contact__content-email">comercial@jussi.com.br</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
